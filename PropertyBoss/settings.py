31b75daeb3a93ab062dbc2dc8c5e5c86c01a7f94
von2020 <orumwensey@gmail.com>
2021-06-09 11:56:26 +0100

    PropertyBoss First Commit

--- PropertyBoss/settings.py
+++ PropertyBoss/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rrurlv+r%nd@7q#gs0eip95vr89bkbn6)99*85u#)ozo8!uhfb'
+# SECRET_KEY = 'rrurlv+r%nd@7q#gs0eip95vr89bkbn6)99*85u#)ozo8!uhfb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,3 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate django-heroku
+django_heroku.settings(locals())
